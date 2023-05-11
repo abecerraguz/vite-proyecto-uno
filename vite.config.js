d2e6521e7d8a2460d82ba848c5530612337fca0e
abecerraguz <alejandro.becerra@publimetro.cl>
2023-05-10 21:41:22 -0400

    Se guardan los cambios

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+
+    // Llamamos a la dependencia
+    import legacy from '@vitejs/plugin-legacy';
+    // Sacamos el metodo defineConfig
+    import { defineConfig } from 'vite';
+    // Dejamos que la función sea exportable
+    export default defineConfig({
+        // Se configura el plugin para retro compatibilidad
+        plugins: [
+            legacy({
+            polyfills: ['es.promise.finally', 'es/map', 'es/set'],
+            modernPolyfills: ['es.promise.finally']
+            })
+        ],
+        // Se agrega la base carpeta donde se encuentra el proyecto
+        base: '/vite-proyecto-uno/',
+    });
